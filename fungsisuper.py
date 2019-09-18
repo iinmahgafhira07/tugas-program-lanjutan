e91de6c49117df661510e5b265c1a510f3ec7235
iinmahgafhira07 <49119136+iinmahgafhira07@users.noreply.github.com>
2019-09-18 03:02:12 -0700

    Create fungsisuper.py

---
+++ fungsisuper.py
@@ -0,0 +1,35 @@
+class induk1(object):
+   def __init__(self, a):
+      self.a = a
+   def cetakA(self):
+      print("nilai a = ",self.a)
+   
+class induk2(object):
+   def __init__(self, b):
+      self.b = b
+   def cetakB(self):
+     print("nilai b = ",self.b)
+     
+     
+class anak(induk1, induk2):
+    def __init__(self, a, b, c):
+   
+      induk1.__init__(self, a)
+      
+      induk2.__init__(self,b)
+      self.c = c
+    def cetakC(self):
+      print("nilai c = ", self.c)
+def main():
+
+  obj = anak(111, 222, 333)
+  
+  obj.cetakA()
+  
+  obj.cetakB()
+  
+  obj.cetakC()
+  
+  
+if __name__ == "__main__":
+  main()
